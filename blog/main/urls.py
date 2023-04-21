35fd59f47bc5d8ce875eb767e4d73ec40c0dc5aa
narendragolla1 <116960868+narendragolla1@users.noreply.github.com>
2023-04-22 03:28:52 +0530

    create article

--- blog/main/urls.py
+++ blog/main/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('article/<int:pk>',views.article,name='get_article'),
     path('author/<int:pk>',views.author,name='get_author'),
+    
+    
+    path('article',views.create_article,name="create_article")
 ]
--- blog/main/views.py
+++ blog/main/views.py
@@ -33,4 +33,23 @@ def author(request, pk):
         'author': author
     }
     return render(request, 'main/author.html', context)
+
+
+def create_article(request):
+    authors=models.Author.objects.all()
+    context={
+        'authors':authors
+    }
+    if (request.method=="POST"):
+        article_data={
+            "title":request.POST['title'],
+            "content":request.POST['content'],
+        
+        }
+        article=models.Article.objects.create(**article_data)
+        author=models.Author.objects.get(pk=request.POST['author'])
+        article.authors.set([author])
+        context['success']=True
+        
     
+    return render(request,'main/create_article.html',context)
